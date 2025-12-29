dff4f02ef35984190afdfca7e0b6fa8885c7fad6
Tiven Wang <i.tiven.wang@gmail.com>
2025-12-29 21:12:08 +0800

    fix: button in radix

--- packages/chatkit-ui/src/components/ui/button.tsx
+++ packages/chatkit-ui/src/components/ui/button.tsx
@@ -44,7 +44,7 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
-  const Comp = asChild ? Slot.Root : "button"
+  const Comp = asChild ? Slot : "button"
 
   return (
     <Comp
